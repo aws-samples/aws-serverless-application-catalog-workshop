2aaccef8a25e2a1fb29fc55428b02e43bfba3d9d
enr1c091 <enrico_bergamo_91@hotmail.com>
2021-01-13 10:53:13 -0300

    Fixed Locust Load Tests to new version

--- sam/nodejs/locust-script.py
+++ sam/nodejs/locust-script.py
@@ -1,16 +1,9 @@
-from locust import TaskSet, task, HttpLocust, between
+from locust import HttpUser, task, between
 
-class ConverterTasks(TaskSet):
-    @task
-    def day_to_hour(self):
-        self.client.get('items/')
-# Receives the URL from CloudFormation output variable containing "/" as last character so omit it as first char 
+class MyUser(HttpUser):
+    wait_time = between(1, 3)
 
-# Add tests to more resources as you require
-#    @task
-#    def day_to_minute(self):
-#        self.client.get('items/1')
-
-class ApiUser(HttpLocust):
-    task_set = ConverterTasks
-    wait_time = between(1,3)
+    @task(1)
+    def index(self):
+        self.client.get("items/")
+        #self.client.get("items/1/")
